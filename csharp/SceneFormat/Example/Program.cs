35fa1277cf11b4e3014c9f3890e7eb92ba55a3e9
Nikita Volobuev <nikitavbv@gmail.com>
2021-04-25 16:18:46 +0000

    feat(csharp): protoc & project base

---
+++ csharp/SceneFormat/Example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Google.Protobuf;
+using SceneFormat;
+
+namespace Example
+{
+    class Example
+    {
+        private static readonly JsonFormatter _jsonFormatter = new JsonFormatter(JsonFormatter.Settings.Default);
+        private static readonly ISceneIO _sceneIO = new SceneIO();
+        
+        static void Main(string[] args)
+        {
+            var scene = new Scene
+            {
+                Version = 42,
+                Cameras =
+                {
+                    new Scene.Types.Camera()
+                    {
+                        Id = 0,
+                        Position = new Scene.Types.Vector3 {
+                            X = 1.01,
+                            Y = 2.76,
+                            Z = 3,
+                        },
+                    }
+                }
+            };
+            
+            _sceneIO.Save(scene, "example_binary.cowscene");
+            _sceneIO.SaveAsJson(scene, "example_json.cowscene");
+
+            var readResultBinary = _sceneIO.Read("example_binary.cowscene");
+            Console.WriteLine($"Camera X is {readResultBinary.Cameras[0].Position.X} when reading binary");
+
+            var readResultJson = _sceneIO.Read("example_json.cowscene");
+            Console.WriteLine($"Camera X is {readResultJson.Cameras[0].Position.X} when reading json");
+        }
+    }
+}

f749d7165c98ed908b783b43963a76207e08f257
Nikita Volobuev <nikitavbv@gmail.com>
2021-05-01 11:59:22 +0300

    Code style fixes

--- ts/example/main.ts
+++ ts/example/main.ts
@@ -1,24 +1,48 @@
-import { Scene, Camera, Vector3, SceneFormatIO } from '@computer-graphics-course/scene-format';
+import { Scene, Camera, Vector3, Transform, SceneFormatIO, SceneObject, Material, SolidMaterial, MeshedObject, PerspectiveCamera } from '@computer-graphics-course/scene-format';
 
 (async () => {
     const scene: Scene = {
-        version: 42,
+        version: 1,
+        renderOptions: {
+            cameraId: 0,
+            width: 1920,
+            height: 1280,
+        },
+        sceneObjects: [<SceneObject> {
+            id: 0,
+            transform: <Transform> {
+                position: <Vector3> { x: 1, y: 1, z: 1},
+            },
+            material: <Material> {
+                solid: <SolidMaterial>{},
+            },
+            meshedObject: <MeshedObject> {
+                reference: 'cow.obj',
+            }
+        }],
+        lights: [],
         cameras: [<Camera>{
             id: 0,
-            position: <Vector3> {
-                x: 1.01,
-                y: 2.76,
-                z: 3,
+            transform: <Transform> {
+                position: <Vector3> {
+                    x: 1.01,
+                    y: 2.76,
+                    z: 3,
+                }
+            },
+            perspective: <PerspectiveCamera> {
+                fov: 60,
             }
-        }]
+        }],
+        materials: [],
     };
     
     await SceneFormatIO.save(scene, 'example_binary.cowscene');
     await SceneFormatIO.saveAsJson(scene, 'example_json.cowscene');
     
     const readResultBinary = await SceneFormatIO.read('example_binary.cowscene');
-    console.log(`Camera X is ${readResultBinary.cameras[0]?.position?.x} when reading binary`);
+    console.log(`Camera X is ${readResultBinary.cameras[0]?.transform?.position?.x} when reading binary`);
 
     const readResultJson = await SceneFormatIO.read('example_json.cowscene');
-    console.log(`Camera X is ${readResultJson.cameras[0]?.position?.x} when reading json`);
+    console.log(`Camera X is ${readResultJson.cameras[0]?.transform?.position?.x} when reading json`);
 })();
--- ts/scene_io.ts
+++ ts/scene_io.ts
@@ -1,6 +1,9 @@
-import { readFile, writeFile } from 'fs';
+import { promises } from 'fs';
 
-import { Scene } from './proto/scene';
+import { Material, Scene } from './proto/scene';
+
+const readFileAsync = promises.readFile;
+const writeFileAsync = promises.writeFile;
 
 export type SceneIO = {
 
@@ -13,6 +16,15 @@ export type SceneIO = {
     saveAsJson: (scene: Scene, path: string) => Promise<void>,
 };
 
+const preprocessScene = (scene: Scene) => {
+    // workaround of what seems to be ts-proto bug.
+    scene.sceneObjects
+        .filter(obj => obj.material != undefined)
+        .forEach(obj => (obj.material as Material).id = '');
+    
+    return scene;
+};
+
 const decode = (data: Buffer | string) => {
     try {
         return Scene.fromJSON(JSON.parse(data.toString()));
@@ -20,36 +32,20 @@ const decode = (data: Buffer | string) => {
         if (Buffer.isBuffer(data)) {
             return Scene.decode(data);
         } else {
-            throw Error('Failed to decode as scene format: input is string, but not a valid json');
+            throw Error('Failed to decode as scene format: input is string, but not a valid json: ' + e);
         }
     }
 };
 
-const encode = (scene: Scene) => Scene.encode(scene).finish();
+const encode = (scene: Scene) => Scene.encode(preprocessScene(scene)).finish();
 const encodeAsJson = (scene: Scene) => JSON.stringify(Scene.toJSON(scene), null, 4);
 
 export const SceneFormatIO : SceneIO = {
 
     decode,
-    read: async (path: string) => new Promise((resolve, reject) => {
-        readFile(path, (err, data) => {
-            if (err) reject(err);
-            resolve(decode(data));
-        });
-    }),
-
+    read: async (path: string) => decode(await readFileAsync(path)),
     encode,
-    save: async (scene: Scene, path: string) => new Promise((resolve, reject) => {
-        writeFile(path, Buffer.from(encode(scene)), err => {
-            if (err) reject(err);
-            resolve();
-        });
-    }),
+    save: async (scene: Scene, path: string) => await writeFileAsync(path, Buffer.from(encode(scene))),
     encodeAsJson,
-    saveAsJson: async (scene: Scene, path: string) => new Promise((resolve, reject) => {
-        writeFile(path, encodeAsJson(scene), err => {
-            if (err) reject(err);
-            resolve();
-        });
-    }),
+    saveAsJson: async (scene: Scene, path: string) => await writeFileAsync(path, encodeAsJson(scene)),
 };
